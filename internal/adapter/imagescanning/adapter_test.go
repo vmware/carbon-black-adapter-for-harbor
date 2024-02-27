c47d706e422f9b324755917be84d60efd57e00ee
Bob Taylor <rtaylor@gci.com>
2024-02-26 17:14:51 -0900

    Fix duplicate value in consumesMimeTypesMetadata
    This fixes a bug where the Adapter reports incorrect values in Capabilities.ConsumesMimeTypes, resulting in Harbor being unable to scan OCI images.  Related adapter metadata tests have also been added.

--- internal/adapter/imagescanning/adapter_test.go
+++ internal/adapter/imagescanning/adapter_test.go
@@ -8,7 +8,38 @@ import (
 	"encoding/base64"
 	"fmt"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	log "github.com/sirupsen/logrus"
+	"github.com/vmware/carbon-black-adapter-for-harbor/internal/model/harbor"
 )
+
+type fakeAdapter struct {
+	// scan []string
+}
+
+func NewFakeAdapter() fakeAdapter {
+	return fakeAdapter{
+		// scan: make([]string, 0),
+	}
+}
+
+func (a fakeAdapter) fakeGetMetadata() harbor.ScannerAdapterMetadata {
+	// this duplicates the Adapter.GetMetadata method
+	// but should use the existing data from Adapter
+	capability := harbor.ScannerCapability{
+		ConsumesMimeTypes: consumesMimeTypesMetadata,
+		ProducesMimeTypes: producesMimeTypesMetadata,
+	}
+
+	scannerMetadata := harbor.ScannerAdapterMetadata{
+		Properties:   propertiesMetadata,
+		Capabilities: []harbor.ScannerCapability{capability},
+		Scanner:      scannerInfo,
+	}
+
+	return scannerMetadata
+}
 
 func TestGetCredential(t *testing.T) {
 
@@ -28,4 +59,46 @@ func TestGetCredential(t *testing.T) {
 		t.Errorf("Pulled getting password")
 	}
 
+}
+
+func TestConsumesMimeTypesMetadata(t *testing.T) {
+	var expectConsumesMimeTypesMetadata = []string{
+		"application/vnd.docker.distribution.manifest.v2+json",
+		"application/vnd.oci.image.manifest.v1+json",
+	}
+
+	ad := NewFakeAdapter()
+	md := ad.fakeGetMetadata()
+
+	if !cmp.Equal(md.Capabilities[0].ConsumesMimeTypes, expectConsumesMimeTypesMetadata) {
+		t.Errorf("ConsumesMimeTypesMetadata did not match expected value %s", expectConsumesMimeTypesMetadata)
+	}
+}
+
+func TestProducesMimeTypesMetadata(t *testing.T) {
+	var expectProducesMimeTypesMetadata = []string{
+		"application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0",
+		"application/vnd.security.vulnerability.report; version=1.1",
+	}
+
+	ad := NewFakeAdapter()
+	md := ad.fakeGetMetadata()
+
+	if !cmp.Equal(md.Capabilities[0].ProducesMimeTypes, expectProducesMimeTypesMetadata) {
+		t.Errorf("ProducesMimeTypesMetadata did not match expected value %s", expectProducesMimeTypesMetadata)
+	}
+}
+
+func TestPropertiesMetadata(t *testing.T) {
+	var expectPropertiesMetadata = map[string]string{
+		"harbor.scanner-adapter/scanner-type": "os-package-vulnerability",
+		"env.LOG_LEVEL":                       log.GetLevel().String(),
+	}
+
+	ad := NewFakeAdapter()
+	md := ad.fakeGetMetadata()
+
+	if !cmp.Equal(md.Properties, expectPropertiesMetadata) {
+		t.Errorf("PropertiesMetadata did not match expected value %s", expectPropertiesMetadata)
+	}
 }
